521cd123ba3be9c24a470f9a938ef030aac41b7f
udugam <ryan.udugam@gmail.com>
2018-08-18 20:45:22 -0400

    Challenge 3 completed

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,5 +1,6 @@
 var inquirer = require('inquirer');
 var mysql = require("mysql");
+var cTable = require("console.table")
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -21,7 +22,7 @@ connection.connect(function(err) {
   });
 
 //This function displays the supervisor's options
-function displaySurpervisorOptions() {
+function displaySupervisorOptions() {
     inquirer
     .prompt([
         {
@@ -42,4 +43,47 @@ function displaySurpervisorOptions() {
                 break
         }
     });
+}
+
+//This function will output all the sales by department into a table
+function viewDepartmentSales() {
+    var query = "SELECT departments.department_id,departments.department_name,departments.over_head_costs, SUM(products.product_sales) AS product_sales, SUM(products.product_sales-departments.over_head_costs) AS total_profit FROM bamazon.departments INNER JOIN products ON departments.department_name = products.department_name GROUP BY department_id,department_name,over_head_costs";
+    connection.query(query, function(err,res) {
+        if(err) {
+            console.log(err)
+        } else {
+            console.table(res)
+        }
+    })
+}
+
+//This function will create a new department
+function createDepartment() {
+    inquirer
+    .prompt([
+        {
+            type:"input",
+            name:"departmentName",
+            message:"Enter the name of the new Department"
+        },
+        {
+            type:"input",
+            name:"overHead",
+            message:"What are the overhead costs of this department?"
+        },
+    ])
+    .then(answer=> {
+        connection.query("INSERT INTO departments SET ?", 
+            {
+                department_name: answer.departmentName,
+                over_head_costs: answer.overHead
+            },
+            function(err,res) {
+                if(err) {
+                    console.log(err)
+                } else {
+                    displaySupervisorOptions()
+                }
+            })
+    })
 }
